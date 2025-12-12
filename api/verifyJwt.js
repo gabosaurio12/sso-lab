ed0f053b7069fcd6af5864f3a62b4dfd0ee288cb
Gabo-Mac <gabgonlo05@gmail.com>
2025-12-11 20:40:01 -0600

    fix jwt

--- api/verifyJwt.js
+++ api/verifyJwt.js
@@ -8,7 +8,7 @@ const client = JwksClient({
 });
 
 function getKey(header, callback) {
-    client.getSigningKey(headir.kid, (err, key) => {
+    client.getSigningKey(header.kid, (err, key) => {
         if (err) return callback(err);
         const signingKey = key.getPublicKey();
         callback(null, signingKey);
